cd9888b4b292115510b4e872a97b235cc73dbe86
Marcus Ball <marcus.ball@live.com>
2013-09-17 17:04:38 -0400

    Reverted old android app; replaced with new one (with a .gitignore)

---
+++ Android/src/com/rogoapp/MainScreenActivity.java
@@ -0,0 +1,23 @@
+package com.rogoapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
+    
+}

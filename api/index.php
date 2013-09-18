39aae1c20405863c81e834076a0a3b02e3ab4b64
Marcus Ball <marcus.ball@live.com>
2013-09-18 13:07:21 -0400

    Added a simple message for our api server page.

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+/* Just going to forward requests for api.rogoapp.com 
+   (with no path/request vars) to the www.rogoapp.com homepage */
+   
+// header("Location: www.rogoapp.com");
+
+echo "Server says, \"hello\".";
+?>
+

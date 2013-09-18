74723c7b1b48d83b6d6e05660c1807f493e7192f
Marcus Ball <marcus.ball@live.com>
2013-09-18 17:11:21 -0400

    Make the API interface have prettier urls; modified test

--- api/operations/test.php
+++ api/operations/test.php
@@ -1,5 +1,12 @@
 <?php
 
-echo json_encode(array('status'=>'success','data'=>'tested!'));
+$data = 'tested!';
+/** If you are good at noticing patterns,        **
+ ** you will quickly see that I have a set of    **
+ ** test cases that I very often repeatedly use. **/
+if(isset($_GET['boobs'])){
+	$data = '( . )( . )';
+}
+echo json_encode(array('status'=>'success','data'=>$data));
 
 ?>
--- api/request.php
+++ api/request.php
@@ -1,16 +1,35 @@
 <?php
 /** This is the page that will handle all incoming api requests **/
 
-require 'require.php';
+require 'require.php'; 
 
-if(!isset($_GET['request_data'])){
+/** Determine and set the output MIME-type **/
+/* This really isn't that necessary for this, but who cares */
+$type = 'application/json'; //Default to the JSON mime-type
+if(isset($_GET['_data_format'])){ //If the data format was specified in the request (***.json/.txt)
+	$format = strtolower($_GET['_data_format']);
+	if($format == 'txt'){
+		$type = 'text/plain';
+	}
+	if($format == 'json'){
+		$type = 'application/json';
+	}
+}
+header('Content-Type: ' . $type);
+/** Done with MIME-type stuff **/
+
+/** Time to actually figure out how to handle the request **/
+if(!isset($_GET['_request_data'])){ //No request data was specified, so output an error
 	echo json_encode(array('status'=>'error','message'=>'No request was specified!'));
 }
-else{
-	$REQUEST = strtolower($_GET['request_data']);
+else{ //Request was specified, so now we just need to find the correct script to pass the data to
+	$REQUEST = strtolower($_GET['_request_data']);
 	if($REQUEST == 'test'){
 		require OP_PATH . 'test.php';
 	}
+	else{ //We would not find any handlers for the request
+		echo json_encode(array('status'=>'error','message'=>'Invalid request object!'));
+	}		
 }
 
 ?>

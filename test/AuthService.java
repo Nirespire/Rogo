b4aae623b7d5dd8a4f8b1e14a5dd464d0a511a37
Jamie <Zyxer22@aol.com>
2013-09-30 14:26:39 -0400

    These folders contain the auth class items I've been working on. Most of test will probably be changed.

---
+++ test/AuthService.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AuthService extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return new Auth(this).getIBinder();
+	}
+
+}

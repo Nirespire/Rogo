51bb760b6eb30533c61b3377420c887980544925
daxkerchner <daxkerchner@hotmail.com>
2013-10-01 11:09:48 -0600

    Added testing capability and basic UserTest

---
+++ Android/tst/com/rogoapp/AbstractTest.java
@@ -0,0 +1,24 @@
+package com.rogoapp;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class AbstractTest {
+
+	static final String FIRST_NAME_DEFAULT = "Manuel";
+	static final String LAST_NAME_DEFAULT = "Bermudez";
+	static final int BIRTHDAY_DEFAULT_YEAR = 43; // 1943
+	static final int BIRTHDAY_DEFAULT_MONTH = 2; // March
+	static final int BIRTHDAY_DEFAULT_DAY = 1; // 1
+	static final Date BIRTHDAY_DEFAULT = new Date(BIRTHDAY_DEFAULT_YEAR, BIRTHDAY_DEFAULT_MONTH, BIRTHDAY_DEFAULT_DAY);
+	static final String EMAIL_DEFAULT = "manuel@example.com";
+
+	static final int SCORE_START = 0;
+	static final int LEVEL_START = 1;
+
+
+}

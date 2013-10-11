a0c1f2a5e6fe2abc49d180e54a29e1c48ec350b7
Sanjay Nair <sanjayhnair@gmail.com>
2013-10-11 13:16:27 -0400

    added createOptionsMenu to all screens

--- Android/src/com/rogoapp/MeetingSomeoneActivity.java
+++ Android/src/com/rogoapp/MeetingSomeoneActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MeetingSomeoneActivity extends Activity {
@@ -12,6 +13,13 @@ public class MeetingSomeoneActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.meeting_someone);
 	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
 
 	public void openSettingsScreen(View v){
 		final Context context = this;
--- Android/src/com/rogoapp/NearYouActivity.java
+++ Android/src/com/rogoapp/NearYouActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +17,13 @@ public class NearYouActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.near_you);
 	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
 	
 	public void openSettingsScreen(View v){
         final Context context = this;

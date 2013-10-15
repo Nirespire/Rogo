95c8fa25a45fbcf540794133610c84e49444c97f
Marcus Ball <marcus.ball@live.com>
2013-10-15 15:41:38 -0400

    Added Windows Phone project

---
+++ WP8/Rogo/ViewModels/TipViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Rogo.ViewModels
+{
+    public class TipViewModel : INotifyPropertyChanged
+    {
+        private string _tipBody;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string TipBody
+        {
+            get
+            {
+                return _tipBody;
+            }
+            set
+            {
+                if (value != _tipBody)
+                {
+                    _tipBody = value;
+                    NotifyPropertyChanged("TipBody");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}

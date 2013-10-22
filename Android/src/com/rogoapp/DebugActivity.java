406d2c53cd8218ebac78dd5306b1d47f5ffc7f76
Sanjay Nair <tkws88@gmail.com>
2013-10-21 22:12:28 -0400

    Working on getting location from gps

--- Android/src/com/rogoapp/DebugActivity.java
+++ Android/src/com/rogoapp/DebugActivity.java
@@ -1,15 +1,23 @@
 package com.rogoapp;
 
-import com.rogoapp.auth.RogoAuthenticatorActivity;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.rogoapp.auth.RogoAuthenticatorActivity;
 
 public class DebugActivity extends Activity {
     
@@ -76,6 +84,22 @@ public class DebugActivity extends Activity {
 
 	 }
 	 
+	 public void addListenerOnButton4() {
+
+	        registerButton = (Button) findViewById(R.id.location_debug_button);
+
+	        registerButton.setOnClickListener(new OnClickListener() {
+
+	            @Override
+	            public void onClick(View arg0) {
+	            	String loc = getLocation(arg0);
+	            	Toast.makeText(DebugActivity.this, loc, Toast.LENGTH_LONG).show();
+	            }
+
+	        });
+
+	 }
+	 
 	 public void openRegistrationScreen(View v){
 	        final Context context = this;
 	        Intent intent = new Intent(context, RegisterActivity.class);
@@ -93,6 +117,43 @@ public class DebugActivity extends Activity {
 	        Intent intent = new Intent(context, MeetingSomeoneActivity.class);
 	        startActivity(intent);
 	 }
+	 
+	 public String getLocation(View v){
+
+			final Context context = this;
+			String bestProvider;
+			List<Address> user = null;
+			double lat;
+			double lng;
+			Geocoder geocoder;
+			String out = "";
+
+			LocationManager loc = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+			Criteria criteria = new Criteria();
+			bestProvider = loc.getBestProvider(criteria, false);
+			Location location = loc.getLastKnownLocation(bestProvider);
+
+			if (location == null){
+				//Toast.makeText(this,"Location Not found",Toast.LENGTH_LONG).show();
+				out = "Location Not found";
+			}else{
+				geocoder = new Geocoder(this);
+				try {
+					user = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+					lat=(double)user.get(0).getLatitude();
+					lng=(double)user.get(0).getLongitude();
+					//Toast.makeText(this," DDD lat: " +lat+",  longitude: "+lng, Toast.LENGTH_LONG).show();
+					System.out.println(" DDD lat: " +lat+",  longitude: "+lng);
+					out = " DDD lat: " +lat+",  longitude: "+lng;
+
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+			return out;
+		}
 	
 	
 

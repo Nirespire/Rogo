c9c43f69002dccbbfb929e0d2a474481d28aa353
Sanjay Nair <sanjayhnair@gmail.com>
2013-11-01 16:53:08 -0400

    met met screen button now does stuff

--- Android/src/com/rogoapp/MeetingSomeoneActivity.java
+++ Android/src/com/rogoapp/MeetingSomeoneActivity.java
@@ -4,8 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MeetingSomeoneActivity extends Activity {
 	@Override
@@ -22,5 +28,30 @@ public class MeetingSomeoneActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_screen, menu);
         return true;
+    }
+	
+	public void weMet(View V){
+	    toaster();
+	    Intent intent = new Intent(getApplicationContext(), MainScreenActivity.class);
+	    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	    startActivity(intent);
+	}
+	
+	public void toaster(){
+        LayoutInflater inflater = getLayoutInflater();
+
+        View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_layout_id));
+
+        ImageView image = (ImageView) layout.findViewById(R.id.image);
+        image.setImageResource(R.drawable.ic_launcher);
+
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText("CONGRATS");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
     }
 }

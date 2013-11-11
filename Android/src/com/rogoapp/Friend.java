8fa4c8fbe25dc88767914c6ea73c2119725e1d85
Anurag Komaravelli <anurag621@yahoo.com>
2013-11-11 15:55:23 -0500

    Finished Friend.java

--- Android/src/com/rogoapp/Friend.java
+++ Android/src/com/rogoapp/Friend.java
@@ -1,5 +1,19 @@
 package com.rogoapp;
 
 public class Friend {
+	
+	// Properties are private
+	private String username;
+	private String firstName;
+	
+	public Friend() {
+		username = "default";
+		firstName = "default";
+	}
+	
+	public Friend(String username, String firstName) {
+		this.username = username;
+		this.firstName = firstName;
+	}
 
 }

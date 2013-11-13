7bf8a8a54833df83434d400b13e4f4fc0984e75d
Sanjay Nair <sanjayhnair@gmail.com>
2013-11-13 13:29:18 -0500

    near your screen should now have settings button

--- Android/src/com/rogoapp/NearYouActivity.java
+++ Android/src/com/rogoapp/NearYouActivity.java
@@ -1,46 +1,66 @@
 package com.rogoapp;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class NearYouActivity extends Activity {
-	
-	Button goToMapButton;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.near_you);
-		
-		goToMapButton = (Button) findViewById(R.id.near_you_map);
+public class NearYouActivity extends SherlockActivity {
+
+    Button goToMapButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.near_you);
+
+        goToMapButton = (Button) findViewById(R.id.near_you_map);
         goToMapButton.setOnClickListener(new OnClickListener() {
-        	
+
             public void onClick(View arg0) {
-            	startActivity(new Intent(NearYouActivity.this, NearYouMapActivity.class));
-           
+                startActivity(new Intent(NearYouActivity.this, NearYouMapActivity.class));
+
             }
         });
 
-	
-	}
-	
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_screen, menu);
-        return true;
+
     }
     
-    public void openUserScreen(View v){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add("User")
+        .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        menu.add("Settings")
+        .setOnMenuItemClickListener(this.SettingsClickListener)
+        .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+
+        return true;
+    }
+
+    public void openUserSettings(View v){
         final Context context = this;
-        Intent intent = new Intent(context, UserActivity.class);
+        Intent intent = new Intent(context, UserSettingsActivity.class);
         startActivity(intent);
     }
+    OnMenuItemClickListener SettingsClickListener = new OnMenuItemClickListener(){
+        @Override
+        public boolean onMenuItemClick(MenuItem item){
+            Intent intent = new Intent(NearYouActivity.this, UserSettingsActivity.class);
+            startActivity(intent);
+            return false;
+        }
+    };
+
+
+    
 }
 

ff4e9797befd526ee38c980ef665308979c02ca0
kwheel <kennethwheeler@ufl.edu>
2013-11-16 15:35:50 -0500

    Map to master, plus some RecentlyMetList stuff with Peter (unfinished)

--- Android/src/com/rogoapp/BuddyListActivity.java
+++ Android/src/com/rogoapp/BuddyListActivity.java
@@ -1,36 +1,32 @@
 package com.rogoapp;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class BuddyListActivity extends Activity {
-	Button addFriend;
 	
-	public void openBuddyListScreen(View v){
-        final Context context = this;
-        Intent intent = new Intent(context, BuddyListActivity.class);
-        startActivity(intent);
-    }
+	private ListView lv;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buddy_list);
 
-        addFriend = (Button) findViewById(R.id.add_friend);
-
-        addFriend.setOnClickListener(new OnClickListener() {
-
-        	@Override
-        	public void onClick(View arg0) {
-                    
-                }
-
-        	});
-    }
+        lv = (ListView) findViewById(R.id.buddylist);
+        // Instantiating an array list (you don't need to do this, you already have yours)
+   //     ArrayList<Friend> recentlyMet = User.getFriends();
+        // This is the array adapter, it takes the context of the activity as a first // parameter, 0the type of list view as a second parameter and your array as a third parameter
+   //     ArrayAdapter<Friend> arrayAdapter =      
+   //     new ArrayAdapter<Friend>(this,android.R.layout.simple_list_item_1, recentlyMet);
+    //    lv.setAdapter(arrayAdapter); 
+    }  
 }

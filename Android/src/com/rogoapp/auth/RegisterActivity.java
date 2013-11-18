052c0840a163d21966909b6c6d016e2be2c8b6f4
Jamie <Zyxer22@aol.com>
2013-11-18 14:41:18 -0500

    Just an update to the screen.

--- Android/src/com/rogoapp/auth/RegisterActivity.java
+++ Android/src/com/rogoapp/auth/RegisterActivity.java
@@ -20,7 +20,6 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 

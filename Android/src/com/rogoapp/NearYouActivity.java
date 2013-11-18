856278e0a1692ca7a30ac89dc653b876b20f835d
Sanjay Nair <tkws88@gmail.com>
2013-11-17 19:12:23 -0500

    near you screen now navigates to send request
    ALSO PULLS USER ID IN AN ABSOLUTELY DISGUSTING AND INEFFICIENT WAY!!!
    :D:D:D

--- Android/src/com/rogoapp/NearYouActivity.java
+++ Android/src/com/rogoapp/NearYouActivity.java
@@ -2,6 +2,7 @@ package com.rogoapp;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.regex.Pattern;
 
 import org.apache.http.NameValuePair;
 import org.json.JSONArray;
@@ -14,6 +15,8 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -26,107 +29,131 @@ import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
 public class NearYouActivity extends SherlockActivity {
 
 	static final String NEARBY_FILE = "nearby";
-	
+
 	Button goToMapButton;
-    ListView nearbyUsersList;
-    LocationManager loc;
+	ListView nearbyUsersList;
+	LocationManager loc;
 
-    ArrayList<String> users;
-    
-    CacheClient cache = new CacheClient(this);
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.near_you);
+	ArrayList<String> users;
 
-        goToMapButton = (Button) findViewById(R.id.near_you_map);
-        goToMapButton.setOnClickListener(new OnClickListener() {
+	CacheClient cache = new CacheClient(this);
 
-            public void onClick(View arg0) {
-                startActivity(new Intent(NearYouActivity.this, NearYouMapActivity.class));
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.near_you);
 
-            }
-        });
-        
-        nearbyUsersList = (ListView) findViewById(R.id.nearby_users_list);
+		goToMapButton = (Button) findViewById(R.id.near_you_map);
+		goToMapButton.setOnClickListener(new OnClickListener() {
 
-        users = new ArrayList<String>();
-        getNearbyUsers();
-        
-        if(!users.isEmpty()){
-        	ArrayAdapter<String> arrayAdapter =      
-        	         new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, users);
-        	         nearbyUsersList.setAdapter(arrayAdapter); 
-        }
+			public void onClick(View arg0) {
+				startActivity(new Intent(NearYouActivity.this, NearYouMapActivity.class));
 
-    }
+			}
+		});
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+		nearbyUsersList = (ListView) findViewById(R.id.nearby_users_list);
 
-        menu.add("User")
-        .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		users = new ArrayList<String>();
+		getNearbyUsers();
 
-        menu.add("Settings")
-        .setOnMenuItemClickListener(this.SettingsClickListener)
-        .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		if(!users.isEmpty()){
+			ArrayAdapter<String> arrayAdapter =      
+					new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, users);
+			nearbyUsersList.setAdapter(arrayAdapter);
+		}
+		
 
-        return true;
-    }
+		nearbyUsersList.setOnItemClickListener(new OnItemClickListener(){
 
-    public void openUserSettings(View v){
-        final Context context = this;
-        Intent intent = new Intent(context, UserSettingsActivity.class);
-        startActivity(intent);
-    }
-    OnMenuItemClickListener SettingsClickListener = new OnMenuItemClickListener(){
-        @Override
-        public boolean onMenuItemClick(MenuItem item){
-            Intent intent = new Intent(NearYouActivity.this, UserSettingsActivity.class);
-            startActivity(intent);
-            return false;
-        }
-    };
-    
-    
-    public boolean getNearbyUsers() {
-        if(ServerClient.isNetworkAvailable()){
-        	JSONObject json = ServerClient.genericPostRequest("nearby", Collections.<NameValuePair>emptyList(), this.getApplicationContext());
-        	if(json != null)
-        		parseJ(json, NEARBY_FILE);
-        	return json != null;
-        }
-        return false;
-    }
-    
-    public void parseJ(JSONObject jObject, String filename){
-        JSONArray jArray = new JSONArray();
-        StringBuffer out;
-        try {
-            jArray = jObject.getJSONArray("data");
-        } catch (JSONException e1) {
-            e1.printStackTrace();
-        }
-        
-        for (int i=0; i < jArray.length(); i++)
-        {
-            out = new StringBuffer();
-        	try {
-                JSONObject oneObject = jArray.getJSONObject(i);
-                // Pulling items from the array
-                out.append("User: " +  oneObject.getString("uid") + 
-                		" Distance: " + oneObject.getString("distance"));
-               
-                
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            
-            users.add(out.toString());
-        }
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id){
 
-    }
+				String user = (String) parent.getItemAtPosition(position);
+				
+				int i = user.indexOf("'");
+				int j = user.lastIndexOf("'");
+				
+				user = user.substring(i + 1 , j);
+				
+				System.out.println(user);
+
+				Intent intent = new Intent(getApplicationContext(), SendRequestActivity.class);
+
+				intent.putExtra("user", user);
+				startActivity(intent);
+			}
+
+
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		menu.add("User")
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+		menu.add("Settings")
+		.setOnMenuItemClickListener(this.SettingsClickListener)
+		.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+
+		return true;
+	}
+
+	public void openUserSettings(View v){
+		final Context context = this;
+		Intent intent = new Intent(context, UserSettingsActivity.class);
+		startActivity(intent);
+	}
+	OnMenuItemClickListener SettingsClickListener = new OnMenuItemClickListener(){
+		@Override
+		public boolean onMenuItemClick(MenuItem item){
+			Intent intent = new Intent(NearYouActivity.this, UserSettingsActivity.class);
+			startActivity(intent);
+			return false;
+		}
+	};
+
+
+	public boolean getNearbyUsers() {
+		if(ServerClient.isNetworkAvailable()){
+			JSONObject json = ServerClient.genericPostRequest("nearby", Collections.<NameValuePair>emptyList(), this.getApplicationContext());
+			if(json != null)
+				parseJ(json, NEARBY_FILE);
+			return json != null;
+		}
+		return false;
+	}
+
+	public void parseJ(JSONObject jObject, String filename){
+		JSONArray jArray = new JSONArray();
+		StringBuffer out;
+		try {
+			jArray = jObject.getJSONArray("data");
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+
+		for (int i=0; i < jArray.length(); i++)
+		{
+			out = new StringBuffer();
+			try {
+				JSONObject oneObject = jArray.getJSONObject(i);
+				// Pulling items from the array
+				out.append("User: " +  "'" + oneObject.getString("uid") + "'" + 
+						" Distance: " + oneObject.getString("distance"));
+
+
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			users.add(out.toString());
+		}
+
+	}
 
 
 

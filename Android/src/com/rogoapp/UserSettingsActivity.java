1165cb3ddca181dd466ce6c0d418f1eaeca09a38
Zyxer22 <Zyxer22@aol.com>
2013-12-12 15:27:56 -0500

    Logout working

--- Android/src/com/rogoapp/UserSettingsActivity.java
+++ Android/src/com/rogoapp/UserSettingsActivity.java
@@ -4,12 +4,16 @@ import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
+import com.rogoapp.auth.RegisterActivity;
 import com.rogoapp.auth.RogoAuthenticatorActivity;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class UserSettingsActivity extends SherlockPreferenceActivity{
+	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -30,8 +34,21 @@ public class UserSettingsActivity extends SherlockPreferenceActivity{
     OnMenuItemClickListener LogoutClickListener = new OnMenuItemClickListener(){
         @Override
         public boolean onMenuItemClick(MenuItem item){
-            RogoAuthenticatorActivity.logout();
-            return false;
+        	AccountManager am = AccountManager.get(SplashScreen.showContext());
+    		Account account = am.getAccountsByType(RogoAuthenticatorActivity.PARAM_AUTHTOKEN_TYPE)[0];
+
+    		//clears the stored password and invalidates the current auth-token if one exists
+    		String authToken = am.peekAuthToken(account, RogoAuthenticatorActivity.PARAM_AUTHTOKEN_TYPE);
+
+    		am.clearPassword(account);
+    		am.invalidateAuthToken(account.type, authToken);
+    		am.removeAccount(account, null, null);
+    		
+    		Intent intent = new Intent(SplashScreen.showContext(), RogoAuthenticatorActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			startActivity(intent);
+    		
+    		return true;
         }
     };
 	

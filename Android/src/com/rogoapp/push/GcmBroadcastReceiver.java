def03c9a1768a0b43dc8fc79d7b3552c027a1e16
Jamie <Zyxer22@aol.com>
2013-12-13 09:45:14 -0500

    I don't git it :?

---
+++ Android/src/com/rogoapp/push/GcmBroadcastReceiver.java
@@ -0,0 +1,12 @@
+package com.rogoapp.push;
+
+import android.content.Intent;
+
+public class GcmBroadcastReceiver {
+
+	public static void completeWakefulIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
